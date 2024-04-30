b5789cf3061c84a98411878d0bc36f66e5e6675d
loftyyyy <lofty08.business@gmail.com>
2024-04-30 22:05:49 +0800

    Hello

---
+++ AI/src/main/java/com/example/ai/CurrentLetter.java
@@ -0,0 +1,24 @@
+package com.example.ai;
+
+public class CurrentLetter{
+
+//    public String currentLetter() {
+//        String currentChar;
+//        clipX.play();
+//        System.out.println(letter);
+//        if (letter % 2 != 0) {
+//            currentChar = "X";
+//            clipX.play();
+//            letterColor = "9DBC98";
+//            letter++;
+//
+//        } else {
+//            clipO.play();
+//            currentChar = "O";
+//            letterColor = "638889";
+//            letter++;
+//        }
+//
+//        return currentChar;
+//    }
+}
--- AI/src/main/java/module-info.java
+++ AI/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.ai {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
 
     opens com.example.ai to javafx.fxml;
