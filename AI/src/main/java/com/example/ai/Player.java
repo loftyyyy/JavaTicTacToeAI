2e70ecaf248ba3d82214637d6aa0c7153786bc91
loftyyyy <lofty08.business@gmail.com>
2024-05-01 22:25:46 +0800

    Heillo

--- AI/src/main/java/com/example/ai/Player.java
+++ AI/src/main/java/com/example/ai/Player.java
@@ -4,6 +4,7 @@ package com.example.ai;
 import javafx.scene.control.Button;
 
 public class Player extends TicTacToeMain{
+    // TODO: Create a Player class that implements a user logic and maybe create a ai class that create a minimax logic
     public Player(){
 
     }
