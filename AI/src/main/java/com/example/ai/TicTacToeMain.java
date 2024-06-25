ceb72caf7e2afb24cfdab4a89cdf7a14ba76bdff
loftyyyy <lofty08.business@gmail.com>
2024-06-26 00:18:00 +0800

    hello

--- AI/src/main/java/com/example/ai/TicTacToeMain.java
+++ AI/src/main/java/com/example/ai/TicTacToeMain.java
@@ -1,16 +1,16 @@
 package com.example.ai;
 
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.media.AudioClip;
 
+import javax.net.ssl.SSLContext;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class TicTacToeMain {
     int letter = 1;
@@ -79,7 +79,18 @@ public class TicTacToeMain {
     protected List<Button> C8 = new ArrayList<>();
 
     protected List<Button> test = new ArrayList<>();
+    protected List<Button> inA11 = new ArrayList<>();
+    protected List<Button> inA12 = new ArrayList<>();
+    protected List<Button> inA13 = new ArrayList<>();
+    protected List<Button> inA21 = new ArrayList<>();
+    protected List<Button> inA22 = new ArrayList<>();
+    protected List<Button> inA23 = new ArrayList<>();
+    protected List<Button> inA31 = new ArrayList<>();
+    protected List<Button> inA32 = new ArrayList<>();
+    protected List<Button> inA33 = new ArrayList<>();
 
+
+    protected  List<Button> inEverything = new ArrayList<>();
 //    protected Button[] C2 = {A21,A22,A23};
 //    protected Button[] C3 = {A31,A32,A33};
 //    protected Button[] C4 = {A11,A21,A31};
@@ -94,15 +105,29 @@ public class TicTacToeMain {
 
 
 
-    public void buttonClick(){
-        for(Button button : test){
+    enum Cell{A11, A12, A13, A21, A22, A23, A31, A32, A33}
+    enum Combination {C1, C2, C3, C4, C5, C6, C7, C8};
 
-            button.setText("j");
+    protected Map<Cell, Set<Combination>> cellToCombination = new HashMap<>();
+    public void buttonClick(ActionEvent e){
+//        Button btn = (Button) e.getSource();
+//        btn.setText("h");
+        for(Map.Entry<Cell, Set<Combination>> entry : cellToCombination.entrySet()){
+            Cell cell = entry.getKey();
+            Set<Combination> combinations = entry.getValue();
+
+            // Do something with the cell and combinations
+            System.out.println("Cell: " + cell + ", Combinations: " + combinations);
 
         }
 
+
+
     }
     public void checkWinner(){
+        for(Button btn : test){
+            System.out.println(btn.getText());
+        }
 
 
     }
@@ -112,6 +137,8 @@ public class TicTacToeMain {
 
     @FXML
     private void initialize(){
+        cellToCombination.put(Cell.A11, Set.of(Combination.C1, Combination.C4, Combination.C7));
+
         C1.add(A11);
         C1.add(A12);
         C1.add(A13);
@@ -144,7 +171,57 @@ public class TicTacToeMain {
         C8.add(A22);
         C8.add(A31);
 
+        test.addAll(C1);
         test.addAll(C2);
+        test.addAll(C3);
+        test.addAll(C4);
+        test.addAll(C5);
+        test.addAll(C6);
+        test.addAll(C7);
+        test.addAll(C8);
+
+        inA11.addAll(C1);
+        inA11.addAll(C4);
+        inA11.addAll(C7);
+
+        inA12.addAll(C1);
+        inA12.addAll(C5);
+
+        inA13.addAll(C1);
+        inA13.addAll(C6);
+        inA13.addAll(C8);
+
+        inA21.addAll(C2);
+        inA21.addAll(C4);
+
+        inA22.addAll(C2);
+        inA22.addAll(C5);
+        inA22.addAll(C7);
+        inA22.addAll(C8);
+
+        inA23.addAll(C2);
+        inA23.addAll(C6);
+
+        inA31.addAll(C3);
+        inA31.addAll(C4);
+        inA31.addAll(C8);
+
+        inA32.addAll(C3);
+        inA32.addAll(C5);
+
+        inA33.addAll(C3);
+        inA33.addAll(C6);
+        inA33.addAll(C7);
+
+        inEverything.addAll(inA11);
+        inEverything.addAll(inA12);
+        inEverything.addAll(inA13);
+        inEverything.addAll(inA21);
+        inEverything.addAll(inA22);
+        inEverything.addAll(inA23);
+        inEverything.addAll(inA31);
+        inEverything.addAll(inA32);
+        inEverything.addAll(inA33);
 
 //    protected Button[] C5 = {A12,A22,A32};
 //    protected Button[] C6 = {A13,A23,A33};
